51d8713fe7eeeb652f924e5107275b5924617c7d
Rodrigo Lima-Lopes <rll307@unicamp.br>
2021-09-21 12:25:11 -0300

    Minor Corrections TidyText

--- Module_2/05_TidyText_functions.R
+++ Module_2/05_TidyText_functions.R
@@ -1,13 +1,15 @@
 # Functions
 Clean_String <- function(text){
   #Clear accents and diacritics
-  temp <- abjutils::rm_accent(text)
+  # temp <- abjutils::rm_accent(text)
   # Clear html 
+  temp <- text
   temp <- textclean::replace_html(temp, symbol = TRUE)
+  temp <- gsub('[[:digit:]]+', ' ', temp)
   # Lowercase
   temp <- tolower(temp)
   # Remove everything that is not a number or letter). 
-  # temp <- stringr::str_replace_all(temp,"[^a-zA-Z\\s]", "")
+  #temp <- stringr::str_replace_all(temp,"[^a-zA-Z\\s]", "")
   # Shrink down to just one white space
   temp <- str_squish(temp)
   # remove empty lines
